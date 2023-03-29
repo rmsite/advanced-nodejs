074d677ce4e87c5823f7d27a5aef771abdcddd8e
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-03-29 11:33:15 -0300

    add facebook authentication feature

---
+++ src/domain/features/facebook-authentication.ts
@@ -0,0 +1,14 @@
+import { type AuthenticationError } from '@/domain/errors'
+import { type AccessToken } from '@/domain/models'
+
+export interface FaceBookAuthentication {
+  perform: (params: FaceBookAuthentication.Params) => Promise<FaceBookAuthentication.Result>
+}
+
+namespace FaceBookAuthentication {
+  export type Params = {
+    token: string
+  }
+
+  export type Result = AccessToken | AuthenticationError
+}
