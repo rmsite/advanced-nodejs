65e802d6c6524122dd79f247e18f0bd870ff8d52
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-04-03 16:26:59 -0300

    ensure AxiosHttpClient rethrows if axios throws

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -4,13 +4,6 @@ import axios from 'axios'
 
 jest.mock('axios')
 
-class AxiosHttpClient {
-  async get <T = any> (args: HttpGetClient.Params): Promise<T> {
-    const result = await axios.get(args.url, { params: args.params })
-    return result.data
-  }
-}
-
 describe('AxiosHttpClient', () => {
   let sut: AxiosHttpClient
   let fakeAxios: jest.Mocked<typeof axios>
@@ -30,16 +23,27 @@ describe('AxiosHttpClient', () => {
   beforeEach(() => {
     sut = new AxiosHttpClient()
   })
+
   describe('get', () => {
     it('Should call get with correct params', async () => {
       await sut.get({ url, params })
+
       expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
     })
-  })
 
-  it('Should return data on success', async () => {
-    const result = await sut.get({ url, params })
-    expect(result).toEqual('any_data')
+    it('Should return data on success', async () => {
+      const result = await sut.get({ url, params })
+
+      expect(result).toEqual('any_data')
+    })
+
+    it('Should rethrow if get throws', async () => {
+      fakeAxios.get.mockRejectedValueOnce(new Error('http_error'))
+
+      const promise = sut.get({ url, params })
+
+      await expect(promise).rejects.toThrow(new Error('http_error'))
+    })
   })
 })
