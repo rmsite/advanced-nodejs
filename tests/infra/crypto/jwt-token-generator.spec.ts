25eab47cad44961b74be8d2a8b4e936dd2ae607a
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-04-03 16:46:52 -0300

    isolate sut creation

---
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -0,0 +1,23 @@
+import { type TokenGenerator } from '@/data/contracts/crypto'
+import jwt from 'jsonwebtoken'
+
+jest.mock('jsonwebtoken')
+
+class JwtTokenGenerator {
+  constructor (private readonly secret: string) {}
+
+  async generateToken (params: TokenGenerator.Params): Promise<void> {
+    const expiraionInSeconds = params.expirationInMs / 1000
+    jwt.sign({ key: params.key }, this.secret, { expiresIn: expiraionInSeconds })
+  }
+}
+describe('JwtTokenGenerator', () => {
+  it('Should call sign with correct params', async () => {
+    const fakeJwt = jwt as jest.Mocked<typeof jwt>
+    const sut = new JwtTokenGenerator('any_secret')
+
+    await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
+
+    expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', { expiresIn: 1 })
+  })
+})
