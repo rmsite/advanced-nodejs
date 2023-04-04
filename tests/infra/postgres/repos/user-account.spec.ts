089ea12a6dee93c8c219c3cdbd0b501d893287eb
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-04-04 11:44:43 -0300

    ensure PgUserAccountRepository loads account if email exists

---
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -0,0 +1,60 @@
+import { type LoadUserAccountRepository } from '@/data/contracts/repos'
+
+import { newDb } from 'pg-mem'
+import { Column, Entity, PrimaryGeneratedColumn, getRepository } from 'typeorm'
+
+class PgUserAccountRepository {
+  async load (params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
+    const pgUserRepo = getRepository(PgUser)
+    const pgUser = await pgUserRepo.findOneBy({ email: params.email })
+    if (pgUser !== undefined && pgUser !== null) {
+      return {
+        id: pgUser.id.toString(),
+        name: pgUser.name ?? undefined
+      }
+    }
+  }
+}
+
+@Entity({ name: 'usuarios' })
+class PgUser {
+  @PrimaryGeneratedColumn()
+    id!: number
+
+  @Column({ name: 'nome', nullable: true })
+    name?: string
+
+  @Column()
+    email!: string
+
+  @Column({ name: 'id_facebook', nullable: true })
+    facebookId?: string
+}
+
+describe('PgUserAccountRepository', () => {
+  describe('load', () => {
+    it('Should return an account if email exists', async () => {
+      const db = newDb({ autoCreateForeignKeyIndices: true })
+      db.public.registerFunction({
+        implementation: () => 'test',
+        name: 'current_database'
+      })
+      db.public.registerFunction({
+        implementation: () => 'test',
+        name: 'version'
+      })
+      const connection = await db.adapters.createTypeormConnection({
+        type: 'postgres',
+        entities: [PgUser]
+      })
+      await connection.synchronize()
+      const pgUserRepo = getRepository(PgUser)
+      await pgUserRepo.save({ email: 'existing_email' })
+      const sut = new PgUserAccountRepository()
+
+      const account = await sut.load({ email: 'existing_email' })
+
+      expect(account).toEqual({ id: '1' })
+    })
+  })
+})
