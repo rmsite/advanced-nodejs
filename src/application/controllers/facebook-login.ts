ee38602b4a7c8133925d5cf0f2358e407156a0b9
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-04-05 15:22:13 -0300

    apply unauthorized helper

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,7 +1,7 @@
 import { type FaceBookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
 import { RequiredFieldError, ServerError } from '@/application/errors'
-import { badRequest, type HttpResponse } from '@/application/helpers'
+import { badRequest, unauthorized, type HttpResponse } from '@/application/helpers'
 
 export class FacebookLoginController {
   constructor (private readonly facebookAuthentication: FaceBookAuthentication) {}
@@ -11,19 +11,16 @@ export class FacebookLoginController {
       if (httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
         return badRequest(new RequiredFieldError('token'))
       }
-      const result = await this.facebookAuthentication.perform({ token: httpRequest.token })
-      if (result instanceof AccessToken) {
+      const accessToken = await this.facebookAuthentication.perform({ token: httpRequest.token })
+      if (accessToken instanceof AccessToken) {
         return {
           statusCode: 200,
           data: {
-            accessToken: result.value
+            accessToken: accessToken.value
           }
         }
       } else {
-        return {
-          statusCode: 401,
-          data: result
-        }
+        return unauthorized()
       }
     } catch (error) {
       return {
