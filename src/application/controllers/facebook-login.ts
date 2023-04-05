e55f991e21c3a62928675dddaee8d7cab3bc4b71
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-04-05 15:17:28 -0300

    apply badRequest helper

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,7 +1,7 @@
 import { type FaceBookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
-import { ServerError } from '@/application/errors'
-import { type HttpResponse } from '@/application/helpers'
+import { RequiredFieldError, ServerError } from '@/application/errors'
+import { badRequest, type HttpResponse } from '@/application/helpers'
 
 export class FacebookLoginController {
   constructor (private readonly facebookAuthentication: FaceBookAuthentication) {}
@@ -9,10 +9,7 @@ export class FacebookLoginController {
   async handle (httpRequest: any): Promise<HttpResponse> {
     try {
       if (httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
-        return {
-          statusCode: 400,
-          data: new Error('The field token is required')
-        }
+        return badRequest(new RequiredFieldError('token'))
       }
       const result = await this.facebookAuthentication.perform({ token: httpRequest.token })
       if (result instanceof AccessToken) {
