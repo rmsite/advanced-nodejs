60133ff3b519487cc5db5c5bd42106a2472808a5
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-04-05 18:13:35 -0300

    isolate sut creation

---
+++ tests/application/controllers/validation/composite.spec.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-confusing-void-expression */
+import { type MockProxy, mock } from 'jest-mock-extended'
+
+interface Validator {
+  validate: () => Error | undefined
+}
+
+class ValidationComposite {
+  constructor (private readonly validators: Validator[]) {}
+
+  validate (): undefined {
+    return undefined
+  }
+}
+
+describe('ValidationComposite', () => {
+  let sut: ValidationComposite
+  let validator1: MockProxy<Validator>
+  let validator2: MockProxy<Validator>
+  let validators: Validator[]
+
+  beforeAll(() => {
+    validator1 = mock<Validator>()
+    validator1.validate.mockReturnValue(undefined)
+    validator2 = mock<Validator>()
+    validator2.validate.mockReturnValue(undefined)
+    validators = [validator1, validator2]
+  })
+
+  beforeEach(() => {
+    sut = new ValidationComposite(validators)
+  })
+
+  it('Should return undefined if all validators return undefined', () => {
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
+})
