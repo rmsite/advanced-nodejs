be321bb9df64a1da6a147cdadc6fa05e2b4dfa70
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-04-06 12:31:49 -0300

    move ValidationBuilder to its own file

---
+++ src/application/validation/builder.ts
@@ -0,0 +1,22 @@
+import { RequiredStringValidator, type Validator } from '@/application/validation'
+
+export class ValidationBuilder {
+  private constructor (
+    private readonly value: string,
+    private readonly fuieldName: string,
+    private readonly validators: Validator[] = []
+  ) {}
+
+  static of (params: { value: string, fieldName: string }): ValidationBuilder {
+    return new ValidationBuilder(params.value, params.fieldName)
+  }
+
+  required (): ValidationBuilder {
+    this.validators.push(new RequiredStringValidator(this.value, this.fuieldName))
+    return this
+  }
+
+  build (): Validator[] {
+    return this.validators
+  }
+}
--- tests/application/controllers/validation/builder.spec.ts
+++ tests/application/controllers/validation/builder.spec.ts
@@ -1,25 +1,4 @@
-import { RequiredStringValidator, type Validator } from '@/application/validation'
-
-class ValidationBuilder {
-  private constructor (
-    private readonly value: string,
-    private readonly fuieldName: string,
-    private readonly validators: Validator[] = []
-  ) {}
-
-  static of (params: { value: string, fieldName: string }): ValidationBuilder {
-    return new ValidationBuilder(params.value, params.fieldName)
-  }
-
-  required (): ValidationBuilder {
-    this.validators.push(new RequiredStringValidator(this.value, this.fuieldName))
-    return this
-  }
-
-  build (): Validator[] {
-    return this.validators
-  }
-}
+import { RequiredStringValidator, ValidationBuilder } from '@/application/validation'
 
 describe('ValidationBuilder', () => {
   it('Should return a RequiredStringValidator', () => {
