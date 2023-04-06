8267eaef0161f03b3dd65f75b66a4a2c86cc0f6b
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-04-06 12:00:18 -0300

    move production code to their own files

--- tests/application/controllers/validation/composite.spec.ts
+++ tests/application/controllers/validation/composite.spec.ts
@@ -1,21 +1,6 @@
+import { ValidationComposite, type Validator } from '@/application/validation'
+
 import { type MockProxy, mock } from 'jest-mock-extended'
-
-interface Validator {
-  validate: () => Error | undefined
-}
-
-class ValidationComposite implements Validator {
-  constructor (private readonly validators: Validator[]) {}
-
-  validate (): Error | undefined {
-    for (const validator of this.validators) {
-      const error = validator.validate()
-      if (error !== undefined) {
-        return error
-      }
-    }
-  }
-}
 
 describe('ValidationComposite', () => {
   let sut: ValidationComposite
