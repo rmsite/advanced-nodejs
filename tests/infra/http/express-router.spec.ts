24beafb29edfda021e65178a76c1ad674a6bd550
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-04-07 15:56:30 -0300

    ensure ExpressRouter calls handle with empty request

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -22,4 +22,15 @@ describe('ExpressRouter', () => {
 
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
   })
+
+  it('Shoul call handle with empty request', async () => {
+    const req = getMockReq()
+    const { res } = getMockRes()
+    const controller = mock<Controller>()
+    const sut = new ExpressRouter(controller)
+
+    await sut.adapt(req, res)
+
+    expect(controller.handle).toHaveBeenCalledWith({})
+  })
 })
