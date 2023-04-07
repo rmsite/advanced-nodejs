02ad955543c707b7a18488fcde9ce15fb50731cd
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-04-07 16:35:11 -0300

    transform ExpressRouter to a function

--- src/infra/http/express-router.ts
+++ src/infra/http/express-router.ts
@@ -1,16 +1,14 @@
 import { type Controller } from '@/application/controllers'
+import { type RequestHandler } from 'express'
 
-import { type Request, type Response } from 'express'
+export const adaptExpressRoute = (controller: Controller): RequestHandler => {
+  return (async (req, res) => {
+    const httpResponse = await controller.handle({ ...req.body })
 
-export class ExpressRouter {
-  constructor (private readonly controller: Controller) {}
-
-  async adapt (req: Request, res: Response): Promise<void> {
-    const httpResponse = await this.controller.handle({ ...req.body })
     if (httpResponse.statusCode === 200) {
       res.status(200).json(httpResponse.data)
     } else {
       res.status(httpResponse.statusCode).json({ error: httpResponse.data.message })
     }
-  }
+  }) as RequestHandler
 }
--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -1,29 +1,33 @@
+/* eslint-disable @typescript-eslint/no-confusing-void-expression */
+/* eslint-disable @typescript-eslint/await-thenable */
 import { type Controller } from '@/application/controllers'
-import { ExpressRouter } from '@/infra/http'
+import { adaptExpressRoute } from '@/infra/http'
 
-import { type Request, type Response } from 'express'
+import { type RequestHandler, type Request, type Response, type NextFunction } from 'express'
 import { getMockReq, getMockRes } from '@jest-mock/express'
 import { type MockProxy, mock } from 'jest-mock-extended'
 
 describe('ExpressRouter', () => {
   let req: Request
   let res: Response
+  let next: NextFunction
   let controller: MockProxy<Controller>
-  let sut: ExpressRouter
+  let sut: RequestHandler
 
   beforeEach(() => {
     req = getMockReq({ body: { any: 'any' } })
     res = getMockRes().res
+    next = getMockRes().next
     controller = mock()
     controller.handle.mockResolvedValue({
       statusCode: 200,
       data: { data: 'any_data' }
     })
-    sut = new ExpressRouter(controller)
+    sut = adaptExpressRoute(controller)
   })
 
   it('Shoul call handle with correct request', async () => {
-    await sut.adapt(req, res)
+    await sut(req, res, next)
 
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
     expect(controller.handle).toHaveBeenCalledTimes(1)
@@ -32,14 +36,14 @@ describe('ExpressRouter', () => {
   it('Shoul call handle with empty request', async () => {
     const req = getMockReq()
 
-    await sut.adapt(req, res)
+    await sut(req, res, next)
 
     expect(controller.handle).toHaveBeenCalledWith({})
     expect(controller.handle).toHaveBeenCalledTimes(1)
   })
 
   it('Shoul respond with 200 and correct data', async () => {
-    await sut.adapt(req, res)
+    await sut(req, res, next)
 
     expect(res.status).toHaveBeenCalledWith(200)
     expect(controller.handle).toHaveBeenCalledTimes(1)
@@ -52,7 +56,7 @@ describe('ExpressRouter', () => {
       statusCode: 400,
       data: new Error('any_error')
     })
-    await sut.adapt(req, res)
+    await sut(req, res, next)
 
     expect(res.status).toHaveBeenCalledWith(400)
     expect(controller.handle).toHaveBeenCalledTimes(1)
@@ -65,7 +69,7 @@ describe('ExpressRouter', () => {
       statusCode: 500,
       data: new Error('any_error')
     })
-    await sut.adapt(req, res)
+    await sut(req, res, next)
 
     expect(res.status).toHaveBeenCalledWith(500)
     expect(controller.handle).toHaveBeenCalledTimes(1)
