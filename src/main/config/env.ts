ade6e03ddd8917eea3168a397a380b6ec7e890ae
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-04-10 16:45:48 -0300

    move typeorm congfif to root folder

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -6,3 +6,19 @@ export const env = {
   port: process.env.PORT ?? 8080,
   jwtSecret: process.env.JWT_SECRET ?? 'p!#@$FA1'
 }
+
+/* TODO
+import { DataSource, type DataSourceOptions } from 'typeorm'
+
+const config: DataSourceOptions = {
+  type: 'postgres',
+  host: 'motty.db.elephantsql.com',
+  port: 5432,
+  username: 'ildwvnre',
+  password: 'cMz2508RQg5vsmzK678j3zT5UbwukabL',
+  database: 'ildwvnre',
+  entities: ['dist/infra/postgres/entities/index.js']
+}
+
+export const connectDB = new DataSource(config)
+*/
