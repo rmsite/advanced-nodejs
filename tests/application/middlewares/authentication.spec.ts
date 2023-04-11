2cd32c308480f95301b766de4801d9e623f3d3d4
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-04-11 16:10:46 -0300

    ensure AuthenticationMiddleware calls authorize with correct input

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -1,19 +1,31 @@
 import { forbidden, type HttpResponse } from '@/application/helpers'
 import { ForbiddenError } from '@/application/errors'
+import { RequiredStringValidator } from '@/application/validation'
+import { type Authorize } from '@/domain/use-cases'
 
 type HttpRequest = { authorization: string }
 
 class AuthenticationMiddleware {
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse<Error>> {
-    return forbidden()
+  constructor (private readonly authorize: Authorize) {}
+  async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Error> | undefined> {
+    const error = new RequiredStringValidator(authorization, 'authorization').validate()
+    if (error !== undefined) return forbidden()
+    await this.authorize({ token: authorization })
   }
 }
 
 describe('AuthenticationMiddleware', () => {
   let sut: AuthenticationMiddleware
+  let authorize: jest.Mock
+  let authorization: string
+
+  beforeAll(() => {
+    authorization = 'any_authorization_token'
+    authorize = jest.fn()
+  })
 
   beforeEach(() => {
-    sut = new AuthenticationMiddleware()
+    sut = new AuthenticationMiddleware(authorize)
   })
 
   it('Should return 403 if authorization is empty', async () => {
@@ -35,13 +47,18 @@ describe('AuthenticationMiddleware', () => {
   })
 
   it('Should return 403 if authorization is undefined', async () => {
-    const sut = new AuthenticationMiddleware()
-
     const httpResponse = await sut.handle({ authorization: undefined as any })
 
     expect(httpResponse).toEqual({
       statusCode: 403,
       data: new ForbiddenError()
     })
+  })
+
+  it('Should call authorize with correct input', async () => {
+    await sut.handle({ authorization })
+
+    expect(authorize).toHaveBeenCalledWith({ token: authorization })
+    expect(authorize).toHaveBeenCalledTimes(1)
   })
 })
