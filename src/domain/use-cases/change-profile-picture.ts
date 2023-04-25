2e0a15c76870c48365274c681bc3a0a7fb7dd27c
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-04-25 15:53:56 -0300

    ensure SavePictureController returns 200 with correct data

--- src/domain/use-cases/change-profile-picture.ts
+++ src/domain/use-cases/change-profile-picture.ts
@@ -4,7 +4,7 @@ import { UserProfile } from '@/domain/entities'
 
 type Setup = (fileStorage: UploadFile & DeleteFile, crypto: UUIDGenerator, userProfileRepo: SaveUserPicture & LoadUserProfile) => ChangeProfilePicture
 type Input = { id: string, file?: Buffer }
-type OutPut = { pistureUrl?: string, initials?: string }
+type OutPut = { pictureUrl?: string, initials?: string }
 export type ChangeProfilePicture = (input: Input) => Promise<OutPut>
 
 export const setupChangeProfilePicture: Setup = (fileStorage, crypto, userProfileRepo) => async ({ id, file }) => {
--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -1,18 +1,19 @@
 import { RequiredFieldError } from '@/application/errors'
-import { type HttpResponse, badRequest } from '@/application/helpers'
+import { type HttpResponse, badRequest, ok } from '@/application/helpers'
 import { type ChangeProfilePicture } from '@/domain/use-cases'
 
 type HttpRequest = { file: { buffer: Buffer, mimeType: string }, userId: string }
-type Model = Error
+type Model = Error | { initials?: string, pictureUrl?: string }
 
 class SavePictureController {
   constructor (private readonly changeProfilePicture: ChangeProfilePicture) {}
-  async handle ({ file, userId }: HttpRequest): Promise<HttpResponse<Model> | undefined> {
+  async handle ({ file, userId }: HttpRequest): Promise<HttpResponse<Model>> {
     if (file === undefined || file === null) return badRequest(new RequiredFieldError('file'))
     if (file.buffer.length === 0) return badRequest(new RequiredFieldError('file'))
     if (!['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimeType)) return badRequest(new InvalidMimeTypeError(['png', 'jpeg']))
     if (file.buffer.length > 5 * 1024 * 1024) return badRequest(new MaxFileSizeError(5))
-    await this.changeProfilePicture({ id: userId, file: file.buffer })
+    const data = await this.changeProfilePicture({ id: userId, file: file.buffer })
+    return ok(data)
   }
 }
 
@@ -43,7 +44,7 @@ describe('SavePictureController', () => {
     mimeType = 'image/png'
     file = { buffer, mimeType }
     userId = 'any_user_id'
-    changeProfilePicture = jest.fn()
+    changeProfilePicture = jest.fn().mockResolvedValue({ initials: 'any_initials', pictureUrl: 'any_picture_url' })
   })
 
   beforeEach(() => {
@@ -127,6 +128,16 @@ describe('SavePictureController', () => {
     await sut.handle({ file, userId })
 
     expect(changeProfilePicture).toHaveBeenCalledWith({ id: userId, file: buffer })
+    expect(changeProfilePicture).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return 200 with valid data', async () => {
+    const httpResponse = await sut.handle({ file, userId })
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      data: { initials: 'any_initials', pictureUrl: 'any_picture_url' }
+    })
     expect(changeProfilePicture).toHaveBeenCalledTimes(1)
   })
 })
