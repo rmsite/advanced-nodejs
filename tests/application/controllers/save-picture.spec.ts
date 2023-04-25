df8fcc3e38979d57ad640cc08604461b08e8526c
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-04-25 14:49:05 -0300

    ensure SavePictureController return 400 if file is not provided

---
+++ tests/application/controllers/save-picture.spec.ts
@@ -0,0 +1,37 @@
+import { RequiredFieldError } from '@/application/errors'
+import { type HttpResponse, badRequest } from '@/application/helpers'
+
+type HttpRequest = { file: any }
+type Model = Error
+
+class SavePictureController {
+  async handle ({ file }: HttpRequest): Promise<HttpResponse<Model>> {
+    return badRequest(new RequiredFieldError('file'))
+  }
+}
+
+describe('SavePictureController', () => {
+  let sut: SavePictureController
+
+  beforeEach(() => {
+    sut = new SavePictureController()
+  })
+
+  it('Should return 400 if file is not provided', async () => {
+    const httpResponse = await sut.handle({ file: undefined })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file')
+    })
+  })
+
+  it('Should return 400 if file is not provided', async () => {
+    const httpResponse = await sut.handle({ file: null })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file')
+    })
+  })
+})
