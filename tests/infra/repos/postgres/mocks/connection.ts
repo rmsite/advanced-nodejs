0335c996282d516b76c9bfd3da56a128b1d8c259
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-06-05 16:06:32 -0300

    fix connection problems

--- tests/infra/repos/postgres/mocks/connection.ts
+++ tests/infra/repos/postgres/mocks/connection.ts
@@ -12,7 +12,7 @@ export const makeFakeDb = async (entities?: any[]): Promise<IMemoryDb> => {
   })
   const connection = await db.adapters.createTypeormConnection({
     type: 'postgres',
-    entities: entities ?? ['src/infra/postgres/entities/index.ts']
+    entities: entities ?? ['src/infra/repos/postgres/entities/index.ts']
   })
   await connection.synchronize()
   return db
