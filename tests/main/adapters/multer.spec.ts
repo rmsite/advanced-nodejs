15267aedf582b7378f0b420ad5392cb52ff09401
Roberto Munhoz <roberto.munhoz@rmsite.com.br>
2023-06-05 14:57:59 -0300

    move MulterAdapter to its own file

--- tests/main/adapters/multer.spec.ts
+++ tests/main/adapters/multer.spec.ts
@@ -1,22 +1,11 @@
 import { ServerError } from '@/application/errors'
+import { adaptMulter } from '@/main/adapters'
+
 import { getMockReq, getMockRes } from '@jest-mock/express'
 import { type RequestHandler, type Request, type Response, type NextFunction } from 'express'
 import multer from 'multer'
 
 jest.mock('multer')
-
-const adaptMulter: RequestHandler = (req, res, next) => {
-  const upload = multer().single('picture')
-  upload(req, res, error => {
-    if (error !== undefined) {
-      return res.status(500).json({ error: new ServerError(error).message })
-    }
-    if (req.file !== undefined) {
-      req.locals = { ...req.locals, file: { buffer: req.file.buffer, mimeType: req.file.mimetype } }
-    }
-    next()
-  })
-}
 
 describe('MultiAdapter', () => {
   let uploadSpy: jest.Mock
